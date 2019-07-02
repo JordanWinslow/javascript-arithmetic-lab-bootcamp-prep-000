ee2ceb2acfa7742b34e6700c41d7d30d801b8c52
Jordan Winslow <admin@jordanwinslow.me>
2019-07-02 04:52:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function dec(n){
   return n -= 1
 }
 function makeInt(n){
-  return n.parseInt(n, 10)
+  return parseInt(n, 10)
 }
 function preserveDecimal (n){
-  return n.parseFloat(n)
+  return parseFloat(n)
 }
